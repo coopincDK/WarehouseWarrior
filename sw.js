edd93517331a3fd0c50eaeb83fb58dbd0377c26c
coopincDK <mrm@smartpack.dk>
2026-02-22 22:54:13 +0100

    jgjghjgjh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'warehouse-warrior-v4.13';
+const CACHE_NAME = 'warehouse-warrior-v4.14';
 
 // Alle filer der skal pre-caches for offline-brug
 // Musik udeladt (for store) — caches on-demand
